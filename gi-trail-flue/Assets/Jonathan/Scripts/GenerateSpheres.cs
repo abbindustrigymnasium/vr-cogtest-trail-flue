dd9dfe6e89caf3e3092de033765237f1a32fbd51
abbjondam <54662731+abbjondam@users.noreply.github.com>
2022-02-01 14:14:21 +0100

    Added spheres to generateSpheres

--- gi-trail-flue/Assets/Jonathan/Scripts/GenerateSpheres.cs
+++ gi-trail-flue/Assets/Jonathan/Scripts/GenerateSpheres.cs
@@ -7,72 +7,110 @@ public class GenerateSpheres : MonoBehaviour
     public GameObject spherePrefab;
     public Material lightMaterial;
     public Material darkMaterial;
+    public Camera playerCamera;
 
+    public class SphereValues
+    {
+        public float rho; 
+        public float theta; 
+        public float phi; 
+        public string color;
+        public string label;
 
-    private float[,,] toGenerate = {{{10,1,1,1},{20,1,1,0},{30,1,1,1},{40,1,1,0},{50,1,1,1},{60,1,1,0}},
-                                    {{10,1,2,1},{20,1,2,0},{30,1,2,1},{40,1,2,0},{50,1,2,1},{60,1,2,0}},
-                                    {{10,2,1,1},{20,2,1,0},{30,2,1,1},{40,2,1,0},{50,2,1,1},{60,2,1,0}},
-                                    {{10,2,2,1},{20,2,2,0},{30,2,2,1},{40,2,2,0},{50,2,2,1},{60,2,2,0}},
-                                    {{10,1,0,1},{20,1,0,0},{30,1,0,1},{40,1,0,0},{50,1,0,1},{60,1,0,0}}
+        public SphereValues(float rhoI, float thetaI, float phiI, string colorI, string labelI)
+        {
+            rho = rhoI;
+            theta = thetaI * Mathf.Deg2Rad;
+            phi =  phiI * Mathf.Deg2Rad;
+            color = colorI;
+            label = labelI;
+        }
+
+    }
+    SphereValues[,] ListOfSphereValues = new SphereValues[,]
+    {
+        {
+            new SphereValues(15,0,90,"white","1"),
+            new SphereValues(15,72,90,"black","A"), 
+            new SphereValues(15,144,90,"white","2"), 
+            new SphereValues(15,216,90,"black","B"), 
+            new SphereValues(15,288,90,"white","3"), 
+        },
+        {
+            new SphereValues(15,0,85,"black","C"),
+            new SphereValues(15,72,85,"white","4"), 
+            new SphereValues(15,144,85,"black","D"), 
+            new SphereValues(15,216,85,"white","5"), 
+            new SphereValues(15,288,85,"black","E"), 
+        },
+        {
+            new SphereValues(15,0,80,"white","6"),
+            new SphereValues(15,72,80,"black","F"), 
+            new SphereValues(15,144,80,"white","7"), 
+            new SphereValues(15,216,80,"black","G"), 
+            new SphereValues(15,288,80,"white","8"), 
+        },
+        {
+            new SphereValues(15,0,75,"black","H"),
+            new SphereValues(15,72,75,"white","9"), 
+            new SphereValues(15,144,75,"black","I"), 
+            new SphereValues(15,216,75,"white","10"), 
+            new SphereValues(15,288,75,"black","J"), 
+        }
     };
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        // spawnSphere(5, 60, 30, 0);
-        // StartCoroutine("Spawn");
+        StartCoroutine("Spawn");
     }
 
-    // Update is called once per 
-    // IEnumerator Spawn(){
-    //     for(var x = 0; x < toGenerate.GetLength(0); x ++){
-    //         for(var y = 0; y < toGenerate.GetLength(1); y ++){
-    //             spawnSphere(toGenerate[x,y,0], toGenerate[x,y,1], toGenerate[x,y,2], (int)toGenerate[x,y,3]);  //Mathf.Deg2Rad
+    IEnumerator Spawn(){
+        for(var x = 0; x < ListOfSphereValues.GetLength(0); x ++){
+            for(var y = 0; y < ListOfSphereValues.GetLength(1); y ++){
+                spawnSphere(ListOfSphereValues[x,y].rho, ListOfSphereValues[x,y].theta, ListOfSphereValues[x,y].phi, ListOfSphereValues[x,y].color, ListOfSphereValues[x,y].label);  
                 
-    //         }
-    //         yield return new WaitForSeconds(1.0f);
-    //         GameObject[] objects = GameObject.FindGameObjectsWithTag("sphere");
-
-    //         for (int i=0; i < objects.Length; i++){
-    //             Destroy(objects[i]);
-    //         }
-
-    //     }
-    // }
-    void Update()
-    {
-        
-
-        if (Input.GetKeyDown("space"))
-        {
-            GameObject[] objects = GameObject.FindGameObjectsWithTag("sphere");
-
-            for (int i=0; i<objects.Length; i++){
-                Destroy(objects[i]);
             }
-            for (int i=0; i<Random.Range(6, 15); i++){
-                spawnSphere(Random.Range(10f, 30f), Random.Range(0f,Mathf.PI * 3 /2), Random.Range(0f,Mathf.PI/2), Random.Range(0,2));
+            do 
+            {
+                yield return null;
+            } while (!Input.GetKeyDown("space"));
+
+            GameObject[] spheres = GameObject.FindGameObjectsWithTag("sphere");
+
+            for (int i=0; i < spheres.Length; i++){
+                Destroy(spheres[i]);
+            }
+            GameObject[] lines = GameObject.FindGameObjectsWithTag("line");
+
+            for (int i=0; i < lines.Length; i++){
+                Destroy(lines[i]);
             }
             
 
         }
     }
 
-    void spawnSphere(float rho, float theta, float phi, int colored){
+    void spawnSphere(float rho, float theta, float phi, string color, string label){
         Vector3 spherePosition = new Vector3(rho * Mathf.Sin(phi) * Mathf.Cos(theta), rho * Mathf.Cos(phi), rho * Mathf.Sin(phi) * Mathf.Sin(theta));
         GameObject sphere = Instantiate(spherePrefab, spherePosition, Quaternion.identity) as GameObject;
+        
         sphere.name = "Sphere" + rho + theta + phi;
-        sphere.transform.LookAt(Vector3.zero);
-        var sphereRenderer = sphere.GetComponent<Renderer>();
+        var sphereScript = sphere.GetComponent<TextOnObjectManager>();
+        sphereScript.label = label;
+        sphereScript.playerCamera = playerCamera;
+        // var sphereRenderer = sphere.GetComponent<Renderer>();
 
-        if (colored == 1)
-        {
-            sphereRenderer.material = darkMaterial;
-        }
-         if (colored == 0)
-        {
-            sphereRenderer.material = lightMaterial;
-        }
+        // if (color == "black")
+        // {
+        //     sphereRenderer.material = darkMaterial;
+        // }
+        //  if (color == "white")
+        // {
+        //     sphereRenderer.material = lightMaterial;
+        // }
+        sphere.SetActive(true);
     }
 
 }
